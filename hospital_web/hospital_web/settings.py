f7e4eb5340b1a3299a97adb2df6c88018408c1c2
CatF1she <23521176@gm.uit.edu.vn>
2025-03-25 16:39:15 +0700

    setting.py update

--- hospital_web/hospital_web/settings.py
+++ hospital_web/hospital_web/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',  # Tên cơ sở dữ liệu
         'USER': 'postgres',      # Tên người dùng
-        'PASSWORD': 'PhuongGiang2803',  # Mật khẩu
+        'PASSWORD': '',  # Mật khẩu
         'HOST': 'localhost',    # Địa chỉ máy chủ (localhost nếu trên máy tính của bạn)
         'PORT': '5432',         # Cổng (mặc định là 5432)
     }
