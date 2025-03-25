a193c7e721aa368103e8e7f8cd0614f40bdc0aab
CatF1she <23521176@gm.uit.edu.vn>
2025-03-25 12:27:11 +0700

    commit

--- hospital_web/hospital_web/settings.py
+++ hospital_web/hospital_web/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'hospital_web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hospital_db',  # Tên cơ sở dữ liệu
+        'NAME': 'postgres',  # Tên cơ sở dữ liệu
         'USER': 'postgres',      # Tên người dùng
-        'PASSWORD': 'namanh2508',  # Mật khẩu
+        'PASSWORD': '',  # Mật khẩu
         'HOST': 'localhost',    # Địa chỉ máy chủ (localhost nếu trên máy tính của bạn)
         'PORT': '5432',         # Cổng (mặc định là 5432)
     }
