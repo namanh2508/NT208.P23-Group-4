5ceda2c34f2424551594d18a3739a85714109db1
giang2005 <23520407@gm.uit.edu.vn>
2025-04-01 15:00:34 +0700

    clean

--- hospital_web/hospital_web/settings.py
+++ hospital_web/hospital_web/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'hospital_db',  # Tên cơ sở dữ liệu
-        'USER': '',      # Tên người dùng root
-        'PASSWORD': '',   # Mật khẩu root_pw
+        'USER': 'postgres',      # Tên người dùng root
+        'PASSWORD': 'giang2005@',   # Mật khẩu root_pw
         'HOST': 'localhost',    # Địa chỉ máy chủ (localhost nếu trên máy tính của bạn)
         'PORT': '5432',         # Cổng (mặc định là 5432)
     }
