8283a21853a475d2a8b4cea906f9da603055bc9d
CatF1she <23521176@gm.uit.edu.vn>
2025-06-12 19:48:16 +0700

    SITE_URL update

--- be/hospital_web/hospital_web/settings.py
+++ be/hospital_web/hospital_web/settings.py
@@ -185,7 +185,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend'
 ]
 SITE_ID = 1  # Required by django-allauth
-SITE_URL = 'http://www.lekhoiblog.id.vn'  # Used for building redirect_uri
+SITE_URL = 'https://lekhoiblog.id.vn'  # Used for building redirect_uri
 
 # Set a secure session cookie
 
